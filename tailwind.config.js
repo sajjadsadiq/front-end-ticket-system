191f5309544fb676994377036e688cdfce346c03
sajjadsadiq <sajjadsadiq2020@gmail.com>
2022-10-17 12:55:08 +0600

    basic project structure

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FE634E",
+          secondary: "#2BC155",
+          accent: "#37cdbe",
+          neutral: "#1E1E1ECC",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
